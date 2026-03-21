915e51db16686ae6c40877f325673c75ff0bbb1d
Abhay S N <abhay182005@gmail.com>
2026-03-21 16:23:57 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Mlops project which detects sentiments from youtube comments',
+    author='Abhay',
+    license='',
+)
